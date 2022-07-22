dd58d8f9343ae08a61b67868c172aa32bbeb33fe
LiubovPil <alexandraelle95@gmail.com>
2022-07-22 15:38:42 +0300

    CountryCapitals finish

--- CountryCapitals/CountryCapitals/CountryCapitals.cpp
+++ CountryCapitals/CountryCapitals/CountryCapitals.cpp
@@ -1,20 +1,74 @@
-﻿// CountryCapitals.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// Tasks from the course "Fundamentals of C++: White Belt"
+// Done by Liubov Piliagina
 
 #include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int q = 0; // count of request
+    cin >> q;
+
+    map<string, string> counties_capitals;
+
+    for (int i = 0; i < q; ++i) {
+        string requested_operation;
+        cin >> requested_operation;
+
+        if (requested_operation == "CHANGE_CAPITAL") {
+            string country_value, new_capital_value;
+            cin >> country_value >> new_capital_value;
+            
+            if (counties_capitals.count(country_value) == 0) {
+                counties_capitals[country_value] = new_capital_value;
+                cout << "Introduce new " << country_value << " with capital " << new_capital_value << endl;
+            }
+            else if (counties_capitals[country_value] == new_capital_value) {
+                cout << "Country " << country_value << " hasn't changed its capital" << endl;
+            }
+            else {
+                string old_capital_value = counties_capitals[country_value];
+                counties_capitals[country_value] = new_capital_value;
+                cout << "Country " << country_value << " has changed its capital from " << old_capital_value << " to " << new_capital_value << endl;
+            }
+        }
+        else if (requested_operation == "RENAME") {
+            string old_country_value, new_country_value;
+            cin >> old_country_value >> new_country_value;
+
+            if (counties_capitals.count(old_country_value) == 1 && counties_capitals.count(new_country_value) == 0) {
+                counties_capitals[new_country_value] = counties_capitals[old_country_value];
+                counties_capitals.erase(old_country_value);
+                cout << "Country " << old_country_value << " with capital " << counties_capitals[new_country_value] << " has been renamed to " << new_country_value << endl;
+            }
+            else {
+                cout << "Incorrect rename, skip " << endl;
+            }
+        }
+        else if (requested_operation == "ABOUT") {
+            string country_value;
+            cin >> country_value;
+            if (counties_capitals.count(country_value) == 0) {
+                cout << "Country " << country_value << " does't exist" << endl;
+            }
+            else {
+                cout << "Country " << country_value << " has capital " << counties_capitals[country_value] << endl;
+            }
+        }
+        else if (requested_operation == "DUMP") {
+            if (counties_capitals.size() == 0) {
+                cout << "There no countries in the world" << endl;
+            }
+            else {
+                for (const auto& item : counties_capitals){
+                    cout << item.first << "/" << item.second << " ";
+                }
+                cout << endl;
+            }
+        }
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
