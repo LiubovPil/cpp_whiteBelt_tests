1bbf3ce55d7220383ade36c17322a53e51f143ed
LiubovPil <alexandraelle95@gmail.com>
2022-07-26 17:03:57 +0300

    Dairy Finish

--- Dairy/Dairy/Dairy.cpp
+++ Dairy/Dairy/Dairy.cpp
@@ -1,20 +1,67 @@
-﻿// Dairy.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// Tasks from the course "Fundamentals of C++: White Belt"
+// Done by Liubov Piliagina
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const vector<int> months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int q;
+    cin >> q;
+
+    int month = 0;
+    vector<vector<string>> dairy_with_tasks(months[month]);
+
+    for (int i = 0; i < q; ++q) {
+        string requested_operation;
+        cin >> requested_operation;
+
+        if (requested_operation == "ADD") {
+            int day = 0;
+            string task;
+            cin >> day >> task;
+
+            if (day > 0 && day <= months[month]) {
+                --day;
+                dairy_with_tasks[day].push_back(task);
+            }
+            else
+                cout << "There is no such day" << endl;
+        }
+        else if (requested_operation == "NEXT") {
+            int previous_month = month;
+            ++month;
+            if (month >= months.size()) {
+                month = 0;
+            }
+            if (months[month] < months[previous_month]) {
+                vector<string>& last_day = dairy_with_tasks[months[month] - 1];
+                for (int i = months[month]; i < months[previous_month]; ++i) {
+                    last_day.insert(end(last_day), begin(dairy_with_tasks[i]), end(dairy_with_tasks[i]));
+                }
+            }
+            dairy_with_tasks.resize(months[month]);
+        }
+        else if (requested_operation == "DUMP") {
+            int day = 0;
+            cin >> day;
+            if (day <= 0 || day > 31) {
+                cout << "There is no such day" << endl;
+            }
+            else {
+                --day;
+                cout << dairy_with_tasks[day].size() << ": ";
+                for (const auto& item : dairy_with_tasks[day]) {
+                    cout << item << " ";
+                }
+            }
+        }
+    }
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
